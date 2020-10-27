af5cffe97734d3ec0ec1c8542f558ce62b0c81fe
Jack Hermanson <jack.hermanson@live.com>
2020-10-27 15:47:57 -0600

    10.1 10/27 database, /purchases

---
+++ dbConnectExec.js
@@ -0,0 +1,31 @@
+const sql = require('mssql');
+const appConfig = require('./config');
+
+const config = {
+    user: appConfig.DB.user,
+    password: appConfig.DB.password,
+    server: appConfig.DB.database,
+    database: appConfig.DB.database
+};
+
+async function executeQuery(aQuery) {
+    let connection = await sql.connect(config);
+    let result = await connection.query(aQuery);
+
+    return result.recordset;
+}
+
+module.exports = {
+    executeQuery
+};
+
+/*
+executeQuery(`SELECT p.PurchaseId, p.DateOfPurchase, p.Amount, s.Name, c.CustomerId, c.FirstName, c.LastName, c.Phone, c.Email
+FROM "Purchase" AS p
+INNER JOIN "Customer" AS c
+ON c.CustomerId = p.CustomerId
+INNER JOIN "ShelfStrain" AS ss
+ON ss.ShelfStrainId = p.ShelfStrainId
+INNER JOIN "Strain" AS s
+ON s.StrainId = ss.StrainId; `);
+*/
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
-
+const db = require('./dbConnectExec');
 const app = express();
 
 app.get('/hi', (req, res) => {
     res.send("hello world");
+});
+
+app.get('/purchases', (req, res) => {
+    db.executeQuery(`SELECT p.PurchaseId, p.DateOfPurchase, p.Amount, s.Name, c.CustomerId, c.FirstName, c.LastName, c.Phone, c.Email
+    FROM "Purchase" AS p
+    INNER JOIN "Customer" AS c
+    ON c.CustomerId = p.CustomerId
+    INNER JOIN "ShelfStrain" AS ss
+    ON ss.ShelfStrainId = p.ShelfStrainId
+    INNER JOIN "Strain" AS s
+    ON s.StrainId = ss.StrainId;`)
+    .then(result => {
+        res.status(200).send(result);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).send();
+    });
 });
 
 app.listen(5000, () => {
